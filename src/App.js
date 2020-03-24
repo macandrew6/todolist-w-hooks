265f50181b93bee124bbdc3c57e3bcb7a4d50cf8
macandrew6 <mr.betterandrewmac@gmail.com>
2020-03-24 02:16:52 -0700

    removes unncessary files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+const AppComponent = () => {
+  const [newTodo, setNewTodo] = useState('');
+  
+  function handleNewTodoChange(e) {
+    e.preventDefault();
   }
-}
 
-export default App;
+  return (
+    <div className="Todo-App">
+      <h1>🤘 Todos! 🤘</h1>
+      <form action="">
+        <input 
+          type="text" 
+          placeholder="write todo here..."
+          onChange={handleNewTodoChange}
+        />
+        <ul>
+          <li>buy groceries</li>
+          <li>get gas</li>
+          <li>clean room</li>
+        </ul>
+      </form>
+    </div>
+  );
+};
+
+export default AppComponent;
